ac7521f84db7857688846ce39fc6e9d4a4c17ce9
AIPERIM AKISHEVA <AIPERIM1995@GMAIL.COM>
2022-05-27 22:49:34 -0400

    dealing with decimals , adding more functionality

---
+++ Calculator/Calculator logic.swift
@@ -0,0 +1,9 @@
+//
+//  Calculator logic.swift
+//  Calculator
+//
+//  Created by Aiperim Akisheva on 5/24/22.
+//  Copyright © 2022 London App Brewery. All rights reserved.
+//
+
+import Foundation
